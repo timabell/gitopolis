1b34e7dd35dcf996b02d1e6d79adaae772adbfa5
Tim Abell <t@0x5.uk>
2025-12-27 22:02:22 +0000

    Convert rust upgrade script to mise
    https://mise.jdx.dev/dev-tools/comparison-to-asdf.html

--- upgrade-rust.sh
+++ upgrade-rust.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -v
 set -e # exit on error
-asdf plugin update rust
-latest=`asdf list all rust | tail -n 2`
-echo $latest
-asdf install rust $latest
-asdf set rust $latest
+
+latest=`mise ls-remote rust | grep -P '^\d+\.\d+\.\d+$' | sort --version-sort | tail -n 1`
+echo "Updating to rust@$latest"
+
+mise use "rust@$latest"
+
 cargo test
+
 git commit -i .tool-versions -m "chore: Upgrade build to latest rust"
